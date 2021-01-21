3f5afd9f2cc246e848f789adbda415dba7514527
Mustafa Dalga <mustafadalgaa@gmail.com>
2021-01-21 21:46:11 +0300

    navigation responsive design completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@ import works from './works.js'
 import testimonials from './testimonials.js'
 works();
 testimonials();
+
+
+/* Navigation Menu */
+var nav = document.querySelector('.header__nav__content');
+var hamburger_menu = document.querySelector('.hamburger-menu-wrap')
+var close_icon = document.querySelector('.nav-close-icon');
+hamburger_menu.addEventListener('click', (event) => {
+    nav.classList.add('open')
+});
+close_icon.addEventListener('click', (event) => {
+    nav.classList.remove('open')
+});
